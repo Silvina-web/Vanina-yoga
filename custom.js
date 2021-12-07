f1b98c8125cc8e1288100c57dfdb11753d89cae4
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2021-12-07 13:21:51 -0300

    Inicio

--- custom.js
+++ custom.js
@@ -3,6 +3,18 @@ gsap.registerPlugin(ScrollTrigger);
 const tl= gsap.timeline();
 
 tl.to(".main-conteiner",5,{x:-window.innerWidth})
+  .from(".seccion2 h1",5,{opacity:0,scale:3})
+
+  .to(".main-conteiner",5,{x:-window.innerWidth*2})
+  .from(".seccion3 h1",5,{opacity:0,scale:3})
+
+  .to(".main-conteiner",5,{x:-window.innerWidth*3})
+  .from(".seccion4 h1",5,{opacity:0,scale:3})
+
+  .to(".main-conteiner",5,{x:-window.innerWidth*4})
+  .from(".seccion5 h1",5,{opacity:0,scale:3})
+
+ 
 
 ScrollTrigger.create({
     animation:tl,
