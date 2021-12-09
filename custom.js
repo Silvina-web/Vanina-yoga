79a04978681a81c921af3648ecbece96b179ac1f
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2021-12-09 14:41:37 -0300

    medias

--- custom.js
+++ custom.js
@@ -1,8 +1,74 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const tl= gsap.timeline();
+// const tl= gsap.timeline();
 
-tl.to(".main-conteiner",5,{x:-window.innerWidth})
+// tl.to(".main-conteiner",5,{x:-window.innerWidth})
+//   .from(".seccion2 h1",5,{opacity:0,scale:3})
+
+//   .to(".main-conteiner",5,{x:-window.innerWidth*2})
+//   .from(".seccion3 h1",5,{opacity:0,scale:3})
+
+//   .to(".main-conteiner",5,{x:-window.innerWidth*3})
+//   .from(".seccion4 h1",5,{opacity:0,scale:3})
+
+//   .to(".main-conteiner",5,{x:-window.innerWidth*4})
+//   .from(".seccion5 h1",5,{opacity:0,scale:3})
+
+ 
+
+// ScrollTrigger.create({
+//     animation:tl,
+//     trigger:".main-conteiner",  
+//     start:"center center",
+//     end:"+=4000",
+//     scrub:true,
+//     pin:true
+
+
+// })
+//ScrollTrigger.saveStyles(".seccion1, seccion2, seccion3, seccion4");
+
+
+ScrollTrigger.matchMedia({
+  //desktop
+  "(min-width:800px":function(){
+
+  const tl= gsap.timeline();
+
+  tl.to(".main-conteiner",5,{x:-window.innerWidth})
+  .from(".seccion2 h1",5,{opacity:0,scale:3})
+
+  .to(".main-conteiner",5,{x:-window.innerWidth*2})
+  .from(".seccion3 h1",5,{opacity:0,scale:3})
+
+  .to(".main-conteiner",5,{x:-window.innerWidth*3})
+  .from(".seccion4 h1",5,{opacity:0,scale:3})
+
+  // .to(".main-conteiner",5,{x:-window.innerWidth*4})
+  // .from(".seccion5 h1",5,{opacity:0,scale:3})
+
+ 
+
+ScrollTrigger.create({
+    animation:tl,
+    trigger:".main-conteiner",
+    start:"center center",
+    end:"+=4000",
+    scrub:true,
+    pin:true
+
+
+})
+
+
+  },
+
+
+  //mobile
+  "(max-width:799 px)": function(){
+    const tl= gsap.timeline();
+
+   tl.to(".main-conteiner",5,{x:-window.innerWidth})
   .from(".seccion2 h1",5,{opacity:0,scale:3})
 
   .to(".main-conteiner",5,{x:-window.innerWidth*2})
@@ -22,7 +88,16 @@ ScrollTrigger.create({
     start:"center center",
     end:"+=4000",
     scrub:true,
-    pin:true
+    pin:true,
 
+
+})
+
+
+  },
+
+  "all": function(){
+    
+  }
 
 })
