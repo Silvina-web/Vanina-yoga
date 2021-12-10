dc4061ebbdad4a4d359ef45c7df294e6573c4e36
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2021-12-10 11:48:02 -0300

    acomodando mediasQ

--- custom.js
+++ custom.js
@@ -1,6 +1,34 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// const tl= gsap.timeline();
+const tl= gsap.timeline();
+
+ScrollTrigger.matchMedia({
+  "(min-width:800px)": function(){
+
+    tl.to(".main-conteiner",5,{x:-window.innerWidth})
+  .from(".seccion2 h1",5,{opacity:0,scale:3})
+
+  .to(".main-conteiner",5,{x:-window.innerWidth*2})
+  .from(".seccion3 h1",5,{opacity:0,scale:3})
+
+  .to(".main-conteiner",5,{x:-window.innerWidth*3})
+  .from(".seccion4 h1",5,{opacity:0,scale:3})
+
+ 
+
+ 
+
+ScrollTrigger.create({
+    animation:tl,
+    trigger:".main-conteiner",  
+    start:"center center",
+    end:"+=4000",
+    scrub:true,
+    pin:true,
+
+  })
+}
+});
 
 // tl.to(".main-conteiner",5,{x:-window.innerWidth})
 //   .from(".seccion2 h1",5,{opacity:0,scale:3})
@@ -11,8 +39,7 @@ gsap.registerPlugin(ScrollTrigger);
 //   .to(".main-conteiner",5,{x:-window.innerWidth*3})
 //   .from(".seccion4 h1",5,{opacity:0,scale:3})
 
-//   .to(".main-conteiner",5,{x:-window.innerWidth*4})
-//   .from(".seccion5 h1",5,{opacity:0,scale:3})
+ 
 
  
 
@@ -20,84 +47,9 @@ gsap.registerPlugin(ScrollTrigger);
 //     animation:tl,
 //     trigger:".main-conteiner",  
 //     start:"center center",
-//     end:"+=4000",
+//     end:"+=3000",
 //     scrub:true,
-//     pin:true
+//     pin:true,
 
 
-// })
-//ScrollTrigger.saveStyles(".seccion1, seccion2, seccion3, seccion4");
-
-
-ScrollTrigger.matchMedia({
-  //desktop
-  "(min-width:800px":function(){
-
-  const tl= gsap.timeline();
-
-  tl.to(".main-conteiner",5,{x:-window.innerWidth})
-  .from(".seccion2 h1",5,{opacity:0,scale:3})
-
-  .to(".main-conteiner",5,{x:-window.innerWidth*2})
-  .from(".seccion3 h1",5,{opacity:0,scale:3})
-
-  .to(".main-conteiner",5,{x:-window.innerWidth*3})
-  .from(".seccion4 h1",5,{opacity:0,scale:3})
-
-  // .to(".main-conteiner",5,{x:-window.innerWidth*4})
-  // .from(".seccion5 h1",5,{opacity:0,scale:3})
-
- 
-
-ScrollTrigger.create({
-    animation:tl,
-    trigger:".main-conteiner",
-    start:"center center",
-    end:"+=4000",
-    scrub:true,
-    pin:true
-
-
-})
-
-
-  },
-
-
-  //mobile
-  "(max-width:799 px)": function(){
-    const tl= gsap.timeline();
-
-   tl.to(".main-conteiner",5,{x:-window.innerWidth})
-  .from(".seccion2 h1",5,{opacity:0,scale:3})
-
-  .to(".main-conteiner",5,{x:-window.innerWidth*2})
-  .from(".seccion3 h1",5,{opacity:0,scale:3})
-
-  .to(".main-conteiner",5,{x:-window.innerWidth*3})
-  .from(".seccion4 h1",5,{opacity:0,scale:3})
-
-  .to(".main-conteiner",5,{x:-window.innerWidth*4})
-  .from(".seccion5 h1",5,{opacity:0,scale:3})
-
- 
-
-ScrollTrigger.create({
-    animation:tl,
-    trigger:".main-conteiner",
-    start:"center center",
-    end:"+=4000",
-    scrub:true,
-    pin:true,
-
-
-})
-
-
-  },
-
-  "all": function(){
-    
-  }
-
-})
+// });
